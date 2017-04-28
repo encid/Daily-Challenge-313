b9074318819827cb5b6553970ab7763b81d788f8
encid <ryancav@gmail.com>
2017-04-28 08:46:12 -0400

    Initial commit

--- Daily-Challenge-313/Program.cs
+++ Daily-Challenge-313/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DailyChallenge312
 {
@@ -10,7 +6,36 @@ namespace DailyChallenge312
     {
         static void Main(string[] args)
         {
-                
+            Console.WriteLine("1234 -> {0}", FindNextInt(1234));
+            Console.WriteLine("1243 -> {0}", FindNextInt(1243));
+            Console.WriteLine("123765 -> {0}", FindNextInt(234765));
+            Console.WriteLine("19000 -> {0}", FindNextInt(19000));
+
+            Console.ReadKey();
+        }
+
+        static int FindNextInt(int input)
+        {
+            /* Brute force find next largest integer using same digits as input */
+            
+            char[] origArr = input.ToString().ToCharArray();
+            Array.Sort(origArr);
+            string origStr = null;
+            for (int i = 0; i < origArr.Length; i++)            
+                origStr += origArr[i].ToString();
+
+            for (int j = input + 1; j < int.MaxValue; j++)
+            {
+                char[] nextArr = j.ToString().ToCharArray();
+                Array.Sort(nextArr);
+                string nextStr = null;
+                for (int k = 0; k < nextArr.Length; k++)
+                    nextStr += nextArr[k].ToString();
+                if (origStr == nextStr)
+                    return j;                
+            }
+
+            return -1;
         }
     }
 }
